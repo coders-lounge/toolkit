17b893f82b197225a9eb62fae7599ef1fba22c5c
Matthew Adcock <matthew.n.adcock@gmail.com>
2021-08-14 19:59:13 +0100

    📋 Command Registering

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,29 @@ export const once = true;
 
 export const execute = async (client) => {
     console.log(`${client.user.tag} >> Logged in!`);
+    client.user.setPresence({ activity: { name: 'new releases', type: 'WATCHING' }, status: 'online' });
+
     // log into the database
     await mongo().then(mongoose => { try { console.log('Database >> Connected to mongoose. Database fully operational!') } finally { mongoose.connection.close() } });
-    // set presence
-    client.user.setPresence({ activity: { name: 'new releases', type: 'WATCHING' }, status: 'online' });
+
+    // get commands from collection and map to array
+    const commands = client.commands.map((command) => command.data);
+
+    // register commands
+    await client.guilds.cache.get(process.env.GUILD_ID)?.commands.set(commands);
+
+    // fetch registered commands
+	const cmds = await client.guilds.cache
+		.get(process.env.GUILD_ID)
+		?.commands.fetch();
+
+    // set permissions
+	client.commands
+		.filter((command) => command.permissions)
+		.each(async (command) => {
+			const cmd = await cmds
+				.filter((cmd) => cmd.name === command.data.name)
+				?.first();
+			if (cmd) cmd.permissions?.set({ permissions: command.permissions });
+		});
 };
